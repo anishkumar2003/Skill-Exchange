46a27943714cbb3298841a63b142e6d0752fb997
Anish kumar sharma <kumaranish200a@gmail.com>
2025-09-09 23:44:18 +0530

    fixed build issue

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../Features/Auth/authSlice";
+import authReducer from "../features/auth/authSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
